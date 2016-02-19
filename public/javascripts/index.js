80fdab91e1d83955c707f577ccb3ca5f215051dc
Tohmua <oliver.musgrove-wethey@mayden.co.uk>
2016-02-19 00:09:48 +0000

    make the on click events only fire once for all objects

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -14,25 +14,7 @@ $(function() {
         alert('There was an error please reload the page!');
      });
 
-    $( ".card" ).each(function( index ) {
-        $( this ).click(function () {
-            $.post( "play/" + $( this ).id, { cardid: $( this ).id } )
-             .done(function () {
-                $( this ).remove();
-                $.get( "/draw/answer/1" )
-                 .done(function (data) {
-                    $("body").append("<div id='" + data[0].id + "' class='card'><p>" + data[0].title + "</p></div>")
-                 })
-                 .error(function () {
-                    alert('There was an error please reload the page!');
-                 });
-
-             })
-             .error(function () {
-                alert('There was an error please reload the page!');
-             });
-        });
-    });
+     applyOnClickEvents();
 });
 
 function guid() {
@@ -44,4 +26,27 @@ function s4() {
   return Math.floor((1 + Math.random()) * 0x10000)
     .toString(16)
     .substring(1);
+}
+
+function applyOnClickEvents() {
+    $( ".card" ).each(function( index ) {
+        $( this ).off("click").click(function () {
+            $.post( "play/" + $( this ).id, { cardid: $( this ).id } )
+             .done(function () {
+                $( this ).remove();
+                $.get( "/draw/answer/1" )
+                 .done(function (data) {
+                    $("body").append("<div id='" + data[0].id + "' class='card'><p>" + data[0].title + "</p></div>")
+                    applyOnClickEvents();
+                 })
+                 .error(function () {
+                    alert('There was an error please reload the page!');
+                 });
+
+             })
+             .error(function () {
+                alert('There was an error please reload the page!');
+             });
+        });
+    });
 }
